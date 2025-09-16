13d50f62110431b70f11f6bb3a825c7b3412f9b6
Godspeed715 <chimzagamdaniel@gmail.com>
2025-09-16 17:36:34 +0100

    modified script.js
    added funtionality for output to be on the page

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const API_KEY = 'UNCUHKPRUE8SL758WT53GXN2L'
-const locationData = document.getElementById('location-data')
+
 
 document.querySelector('form').addEventListener('submit', async (event) => {
     event.preventDefault()
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Port Harcourt/?key=${API_KEY}`)
+    const locationData = document.getElementById('location-data').value;
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationData}/?key=${API_KEY}`)
     const data = await response.json()
     const weatherData = data.currentConditions.conditions
     console.log(weatherData)
-
+    document.getElementsByTagName('div')[0].textContent=weatherData
 })
 
 async function getWeatherData(params) {
