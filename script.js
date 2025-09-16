737298919caefb65855c8fbe8ead9366beba63f4
Godspeed715 <chimzagamdaniel@gmail.com>
2025-09-16 12:32:51 +0100

    modified script.js
    added api functionality, input still not working

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const API_KEY = 'UNCUHKPRUE8SL758WT53GXN2L'
+const locationData = document.getElementById('location-data')
 
+document.querySelector('form').addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Port Harcourt/?key=${API_KEY}`)
+    const data = await response.json()
+    const weatherData = data.currentConditions.conditions
+    console.log(weatherData)
+
+})
+
+async function getWeatherData(params) {
+
+}
