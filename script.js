eff514de58dfcb0ca246f49f764711ed5cab544c
Godspeed715 <chimzagamdaniel@gmail.com>
2025-09-18 14:12:16 +0100

    modified script.js
    added error handling for fetching the weather data

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const API_KEY = 'UNCUHKPRUE8SL758WT53GXN2L'
-
-
+const API_KEY = 'UNCUHKPRUE8SL758WT53GXN2L';
+const weatherResult = document.getElementById('weather-result');
 document.querySelector('form').addEventListener('submit', async (event) => {
-    event.preventDefault()
-    const locationData = document.getElementById('location-data').value;
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationData}/?key=${API_KEY}`)
-    const data = await response.json()
-    const weatherData = data.currentConditions.conditions
-    console.log(weatherData)
-    document.getElementsByTagName('div')[0].textContent=weatherData
-})
+	event.preventDefault();
+	// show spinner
+	loading.style.display = 'block';
+	weatherResult.innerHTML = '';
+	const locationData = document.getElementById('location-data').value;
 
-async function getWeatherData(params) {
+	try {
+		const response = await fetch(
+			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationData}/?key=${API_KEY}`
+		);
+		const data = await response.json();
+		const weatherData = data.currentConditions.conditions;
+		weatherResult.textContent = weatherData;
+		console.log(weatherData);
+	} catch (error) {
+		weatherResult.textContent = 'Error, Check Internet Connection';
+	} finally {
+		loading.style.display = 'none';
+	}
+});
 
-}
+async function getWeatherData(params) {}
